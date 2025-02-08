089d26162128a1c25a32cb79234d841125ff7da0
Brad Hardwick <bhardwick@aerotage.com>
2025-02-08 07:51:04 -0500

    Commit HalcoGen Files

--- LED_TOGGLE/Debug/source/subdir_rules.mk
+++ LED_TOGGLE/Debug/source/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 source/%.obj: ../source/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7R5 --code_state=32 --float_support=VFPv3D16 --include_path="C:/Users/Brad Hardwick/Documents/GitHub/ARM-TMS570LC43x-CCS-HALCoGen/LED_TOGGLE/include" --enum_type=packed --abi=eabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7R5 --code_state=32 --float_support=VFPv3D16 --include_path="C:/Users/Brad Hardwick/Documents/GitHub/ARM-TMS570LC43x-CCS-HALCoGen/LED_TOGGLE" --include_path="C:/Users/Brad Hardwick/Documents/GitHub/ARM-TMS570LC43x-CCS-HALCoGen/LED_TOGGLE/include" --enum_type=packed --abi=eabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 source/%.obj: ../source/%.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7R5 --code_state=32 --float_support=VFPv3D16 --include_path="C:/Users/Brad Hardwick/Documents/GitHub/ARM-TMS570LC43x-CCS-HALCoGen/LED_TOGGLE/include" --enum_type=packed --abi=eabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7R5 --code_state=32 --float_support=VFPv3D16 --include_path="C:/Users/Brad Hardwick/Documents/GitHub/ARM-TMS570LC43x-CCS-HALCoGen/LED_TOGGLE" --include_path="C:/Users/Brad Hardwick/Documents/GitHub/ARM-TMS570LC43x-CCS-HALCoGen/LED_TOGGLE/include" --enum_type=packed --abi=eabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
